236790ba512f424ab7a3b411f7a8558e38050a80
Albert Lie <alberttri23@gmail.com>
2022-04-15 13:34:14 +0800

    add barebone getDeployedProjects in projectFactory

--- contracts/ProjectFactory.sol
+++ contracts/ProjectFactory.sol
@@ -9,9 +9,13 @@ contract ProjectFactory {
     event ProjectCreated(address newProject); // Note: you should add additional data fields in this event
 
     function create() external {
-        Project newProject = new Project();
-        deployedProjects.push(newProject);
+        Project project = new Project();
+        deployedProjects.push(project);
 
         emit ProjectCreated(address(0xdeadbeef)); // TODO: replace me with the actual Project's address
+    }
+
+    function getDeployedProjects() public view returns (Project[] memory) {
+        return deployedProjects;
     }
 }
