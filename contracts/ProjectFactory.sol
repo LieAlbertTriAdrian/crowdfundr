fed449b1e3bdc00129d34d797a3bfcdfb2c65f95
Albert Lie <alberttri23@gmail.com>
2022-04-15 16:05:45 +0800

    add field in emitted event

--- contracts/ProjectFactory.sol
+++ contracts/ProjectFactory.sol
@@ -6,14 +6,14 @@ import './Project.sol';
 contract ProjectFactory {
     Project[] public deployedProjects;
 
-    event ProjectCreated(address newProject); // Note: you should add additional data fields in this event
+    event ProjectCreated(address newProject, string randomName); // Note: you should add additional data fields in this event
 
     function create() external {
         // TODO: Remove hardcode parameter for target
         Project project = new Project(msg.sender, 10);
         deployedProjects.push(project);
 
-        emit ProjectCreated(address(0xdeadbeef)); // TODO: replace me with the actual Project's address
+        emit ProjectCreated(address(0xdeadbeef), "HAHAHAHHAHA"); // TODO: replace me with the actual Project's address
     }
 
     function getDeployedProjects() public view returns (Project[] memory) {
