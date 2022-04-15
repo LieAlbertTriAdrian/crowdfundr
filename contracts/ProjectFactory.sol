86effcd30dd76c2172199cbef369b67f761ef192
Albert Lie <alberttri23@gmail.com>
2022-04-15 13:30:36 +0800

    implement barebone create function in projectFactory

--- contracts/ProjectFactory.sol
+++ contracts/ProjectFactory.sol
@@ -1,11 +1,16 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.4;
 
+import './Project.sol';
+
 contract ProjectFactory {
+    Project[] public deployedProjects;
+
     event ProjectCreated(address newProject); // Note: you should add additional data fields in this event
 
     function create() external {
-        // TODO: implement me!
+        Project newProject = new Project();
+        deployedProjects.push(newProject);
 
         emit ProjectCreated(address(0xdeadbeef)); // TODO: replace me with the actual Project's address
     }
