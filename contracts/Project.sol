7bd44e7232401fdcfee678cc229a15f16941f1bb
Albert Lie <alberttri23@gmail.com>
2022-04-15 14:02:36 +0800

    add contribute() in Project

--- contracts/Project.sol
+++ contracts/Project.sol
@@ -2,7 +2,24 @@
 pragma solidity ^0.8.4;
 
 contract Project {
-    // TODO: delete the unneeded storage variable below and
-    // actually implement me!
-    uint256 public foo;
+    address public creator;
+    uint256 public targetAmount;
+    // minimumContribution for all projects by default in ETH
+    // TODO: Change to 0.01 ETH
+    uint256 public constant minimumContribution = 1;
+
+    // Ether held by the contract on behalf of contributors/pledgers
+    mapping(address => uint256) public contributionOf;
+
+    constructor (address _creator, uint256 _targetAmount) public {
+        creator = _creator;
+        targetAmount = _targetAmount;
+    }
+
+    function contribute() public payable {
+        require(msg.value >= minimumContribution);
+        // TODO: Check for deadline of the project
+
+        contributionOf[msg.sender] += msg.value;
+    }
 }
